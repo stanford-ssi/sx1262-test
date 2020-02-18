06726a001031176cf6cc0d63d4403879f1a2e13d
Tim Vrakas <tim.vrakas@gmail.com>
2020-02-18 02:39:37 -0800

    It blinks now

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,24 @@
 
 
 void setup() {
-
+  Serial.begin(9600);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  
 }
 
 void loop(){
-  interrupts();
+  Serial.println("Lol");
+  digitalWrite(1,HIGH);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  delay(500);
+  digitalWrite(1,LOW);
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  delay(500);
 }
--- variants/custom/variant.h
+++ variants/custom/variant.h
@@ -72,37 +72,15 @@ extern "C"
 #define digitalPinHasPWM(P)        ( g_APinDescription[P].ulPWMChannel != NOT_ON_PWM || g_APinDescription[P].ulTCChannel != NOT_ON_TIMER )
 
 /*
- * digitalPinToTimer(..) is AVR-specific and is not defined for SAMD
- * architecture. If you need to check if a pin supports PWM you must
- * use digitalPinHasPWM(..).
- *
- * https://github.com/arduino/Arduino/issues/1833
- */
-// #define digitalPinToTimer(P)
-
-// LEDs
-#define PIN_LED_13           (13u)
-#define PIN_LED_RXL          (25u)
-#define PIN_LED_TXL          (26u)
-#define PIN_LED              PIN_LED_13
-#define PIN_LED2             PIN_LED_RXL
-#define PIN_LED3             PIN_LED_TXL
-#define LED_BUILTIN          PIN_LED_13
-#define PIN_NEOPIXEL         (8)
-
-/*
  * Analog pins
  */
-#define PIN_A0               (14ul)
-#define PIN_A1               (PIN_A0 + 1)
-#define PIN_A2               (PIN_A0 + 2)
-#define PIN_A3               (PIN_A0 + 3)
-#define PIN_A4               (PIN_A0 + 4)
-#define PIN_A5               (PIN_A0 + 5)
-#define PIN_A6               (PIN_A0 + 6)
-
-#define PIN_DAC0             (14ul)
-#define PIN_DAC1             PIN_A1
+#define PIN_A0               (0ul)
+#define PIN_A1               (0ul)
+#define PIN_A2               (0ul)
+#define PIN_A3               (0ul)
+#define PIN_A4               (0ul)
+#define PIN_A5               (0ul)
+#define PIN_A6               (0ul)
 
 static const uint8_t A0  = PIN_A0;
 static const uint8_t A1  = PIN_A1;
@@ -112,88 +90,12 @@ static const uint8_t A4  = PIN_A4;
 static const uint8_t A5  = PIN_A5;
 static const uint8_t A6  = PIN_A6 ;
 
-static const uint8_t DAC0 = PIN_DAC0;
-static const uint8_t DAC1 = PIN_DAC1;
-
-#define ADC_RESOLUTION		12
-
-// Other pins
-#define PIN_ATN              (31ul)
-static const uint8_t ATN = PIN_ATN;
-
-/*
- * Serial interfaces
- */
-
-// Serial1
-#define PIN_SERIAL1_RX       (0ul)
-#define PIN_SERIAL1_TX       (1ul)
-#define PAD_SERIAL1_RX       (SERCOM_RX_PAD_1)
-#define PAD_SERIAL1_TX       (UART_TX_PAD_0)
-
-/*
- * SPI Interfaces
- */
-#define SPI_INTERFACES_COUNT 1
-
-#define PIN_SPI_MISO         (23u)
-#define PIN_SPI_MOSI         (24u)
-#define PIN_SPI_SCK          (25u)
-#define PERIPH_SPI           sercom1
-#define PAD_SPI_TX           SPI_PAD_3_SCK_1
-#define PAD_SPI_RX           SERCOM_RX_PAD_2
-
-static const uint8_t SS	  = 9 ;	// SERCOM1 last PAD is present on d9 but HW SS isn't used. Set here only for reference.
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
-
-/*
- * Wire Interfaces
- */
-#define WIRE_INTERFACES_COUNT 1
-
-#define PIN_WIRE_SDA         (21u)
-#define PIN_WIRE_SCL         (22u)
-#define PERIPH_WIRE          sercom2
-#define WIRE_IT_HANDLER      SERCOM2_Handler
-
-static const uint8_t SDA = PIN_WIRE_SDA;
-static const uint8_t SCL = PIN_WIRE_SCL;
-
 /*
  * USB
  */
-#define PIN_USB_HOST_ENABLE (28ul)
-#define PIN_USB_DM          (29ul)
-#define PIN_USB_DP          (30ul)
-
-/*
- * I2S Interfaces
- */
-#define I2S_INTERFACES_COUNT 1
-
-#define I2S_DEVICE          0
-#define I2S_CLOCK_GENERATOR 3
-
-#define PIN_I2S_SDO          (11u)
-#define PIN_I2S_SDI          (12u)
-#define PIN_I2S_SCK          PIN_SERIAL1_TX
-#define PIN_I2S_FS           (10u)
-#define PIN_I2S_MCK          PIN_SERIAL1_RX
-
-//QSPI Pins
-#define PIN_QSPI_SCK    (34u)
-#define PIN_QSPI_CS     (35u)
-#define PIN_QSPI_IO0    (36u)
-#define PIN_QSPI_IO1    (37u)
-#define PIN_QSPI_IO2    (38u)
-#define PIN_QSPI_IO3    (39u)
-
-#if !defined(VARIANT_QSPI_BAUD_DEFAULT)
-  // TODO: meaningful value for this
-  #define VARIANT_QSPI_BAUD_DEFAULT 5000000
-#endif
+#define PIN_USB_HOST_ENABLE (0ul)
+#define PIN_USB_DM          (0ul)
+#define PIN_USB_DP          (0ul)
 
 #ifdef __cplusplus
 }
